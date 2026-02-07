667a039f431acade44f5cf8ff5e14e9dd33703d4
Perry Huang <perryhuang2015@gmail.com>
2026-02-07 10:11:49 -0500

    feat: add initial routes

---
+++ src/routes.jsx
@@ -0,0 +1,26 @@
+import Login from "./components/Login";
+
+const routes = [
+    {
+        Component: Login,
+        path: "/",
+    },
+    {
+        // Component: Register,
+        // path: "/register",
+    },
+    {
+        // Component: Posts,
+        // path: "/posts",
+    },
+    {
+        // Component: CreatePost,
+        // path: "/posts/new",
+    },
+    {
+        // Component: Post,
+        // path: "/posts/:postId",
+    },
+];
+
+export default routes;
