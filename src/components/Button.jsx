c81bbc84bb4cdd79f401450126039654f0d84dc1
Perry Huang <perryhuang2015@gmail.com>
2026-02-08 14:28:06 -0500

    feat: remove global button style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
-import "./Button.module.css";
+import styles from "./Button.module.css";
 
 function Button({ type, text, className }) {
     return (
         <>
-            <button type={type} className={className}>
+            <button type={type} className={`${styles.button} ${className} `}>
                 {text}
             </button>
         </>
