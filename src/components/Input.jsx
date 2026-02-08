77c995977a8d66b58745a1f87e10bc777c582aa5
Perry Huang <perryhuang2015@gmail.com>
2026-02-08 12:45:04 -0500

    feat: add input styling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,12 @@
-import "./Input.module.css";
+import styles from "./Input.module.css";
 
 function Input({ type, labelName, name, id, defaultValue, defaultChecked }) {
     return (
         <>
-            <p>
-                <label htmlFor={id}>{labelName} </label>
+            <p className={styles.container}>
+                <label htmlFor={id} className={styles.label}>
+                    {labelName}{" "}
+                </label>
                 <input
                     id={id}
                     type={type}
